498514db81e9361d4ae03fce1054299f25a4cb70
austbot <me@austbot.com>
2022-03-07 22:07:39 -0600

    docs update

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -89,7 +89,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Glasseaters, Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
--- packages/docs/src/components/HomepageFeatures/index.tsx
+++ packages/docs/src/components/HomepageFeatures/index.tsx
@@ -4,48 +4,42 @@ import styles from './styles.module.css';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  //Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Wallet of Wallets',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Create a huge wallet with tons of members, sign NFTs and let unlimited of people get a piece of royalties
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'NFT Membership Utility',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Add your NFTs to a Hydra and stream sale royalties to your HodLers
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Token Membership',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Easy Staking rewards for people who hold and stake your token that is connected to a Hydra
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
